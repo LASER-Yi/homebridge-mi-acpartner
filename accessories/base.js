8dab523c19093415ab680ef8b9f09d15f0f33be2
LASER-Yi <ly0007@yeah.net>
2018-02-21 21:46:22 +0800

    1.0.0

--- accessories/base.js
+++ accessories/base.js
@@ -1,14 +1,25 @@
-class Base{
-    constructor(config, platform) {
-        this.init(config,platform);
-    }
+class Base {
+    constructor(config, platform) {}
 
     init(config, platform) {
         this.platform = platform;
         this.log = platform.log;
         this.config = config;
         this.name = config['name'];
-        this.deviceIndex = this.config['deviceIndex'] || 0;
+        //Search device position
+        if (config['deviceIp']) {
+            let index = 0;
+            for (var elem in platform.config.devices) {
+                if (elem == config['deviceIp']) {
+                    this.deviceIndex = index;
+                    break;
+                }
+                index++;
+            }
+        } else {
+            //Or use default value 0
+            this.deviceIndex = 0;
+        }
     }
     getServices() {
         return this.services;
